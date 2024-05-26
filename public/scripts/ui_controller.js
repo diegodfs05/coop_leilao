d91a49c4b31b57696b19dfec45f755daf78d89c0
diegodfs05 <80434998+diegodfs05@users.noreply.github.com>
2024-05-26 20:30:32 +0000

    static homepage

--- public/scripts/ui_controller.js
+++ public/scripts/ui_controller.js
@@ -3,12 +3,15 @@ let navBar = document.getElementById('sect-navbar');
 let floatingMenu = document.querySelector("#sect-navbar .floating-menu");
 let barsSvg = document.querySelector("#sect-navbar .bars-svg");
 let crossSvg = document.querySelector("#sect-navbar .cross-svg");
+let menuBtn = document.querySelector("#sect-navbar .menu-toggle");
+
 function toggleNavbar() {
     floatingMenu.classList.toggle("open");
     barsSvg.classList.toggle("d-none");
     crossSvg.classList.toggle("d-none");
 }
 
+menuBtn.addEventListener('click', toggleNavbar);
 /* -- NAVBAR SCROLL TRIGGER -- */
 window.addEventListener('scroll', function () {
     let scrollPosition = window.scrollY;
@@ -19,4 +22,5 @@ window.addEventListener('scroll', function () {
 
     }
 });
+
 /* -- NAVBAR CONTROLLER -- */
