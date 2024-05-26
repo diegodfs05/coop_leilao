9d367fe87b33a30b83a06cd98c31ba66e65799b8
diegodfs05 <80434998+diegodfs05@users.noreply.github.com>
2024-05-26 00:00:29 +0000

    evolucao dev

--- models/Page.js
+++ models/Page.js
@@ -2,10 +2,15 @@ class Page {
     constructor(title) {
       this.title = title;
       this.navbarItems = [];
+      this.pageContent = [];
+      this.footerContent = [];
     }
   
     addNavbarItem(name, link) {
       this.navbarItems.push({ name, link });
+    }
+    addPageContent(htmlContent) {
+      this.pageContent.push({ htmlContent});
     }
   
     getNavbar() {
